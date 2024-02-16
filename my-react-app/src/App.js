8c39e118d120c0705410d52cfd51e9f57f75a3bf
Sanika <sanika.baghel@prorigosoftware.com>
2024-02-16 17:24:53 +0530

    Added scroll bar and json button CSS

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -38,42 +38,47 @@ const App = () => {
       <NavigationBar /> {/* Include the NavigationBar component here */}
       <div className="row">
         <div className="col-md-2 sidebar">
-          <h8 style={{ color:'white' }}><b>ToolBox</b></h8>
-          <DraggableItem type="BUTTON" text="Button" />
-          <DraggableItem type="TEXTBOX" text="Textbox" />
-          <DraggableItem type="RADIO" text="Radio Button" />
-          <DraggableItem type="CHECKBOX" text="Checkbox" />
-          <DraggableItem type="DROPDOWN" text="Catalog" />
-          <DraggableItem type="LOOKUP" text="Lookup" />
-          <DraggableItem type="TEXTAREA" text="Textarea" />
-          <DraggableItem type="CALENDER" text="Calender" />
-          <DraggableItem type="BARCODE" text="Barcode" />
-          <DraggableItem type="ATTACHMENT" text="Attachment" />
-          <DraggableItem type="LOOKUPANDBARCODE" text="Lookup & Barcode" />
-          <DraggableItem type="HEADER" text="Header" />
-          <DraggableItem type="FOOTER" text="Footer" />
+          <div style={{ overflowY: 'auto', maxHeight: '380px' }}>
+            <h8 style={{ color: 'white' }}><b>ToolBox</b></h8>
+            <DraggableItem type="BUTTON" text="Button" />
+            <DraggableItem type="TEXTBOX" text="Textbox" />
+            <DraggableItem type="RADIO" text="Radio Button" />
+            <DraggableItem type="CHECKBOX" text="Checkbox" />
+            <DraggableItem type="DROPDOWN" text="Catalog" />
+            <DraggableItem type="LOOKUP" text="Lookup" />
+            <DraggableItem type="TEXTAREA" text="Textarea" />
+            <DraggableItem type="CALENDAR" text="Calendar" />
+            <DraggableItem type="BARCODE" text="Barcode" />
+            <DraggableItem type="ATTACHMENT" text="Attachment" />
+            <DraggableItem type="LOOKUPANDBARCODE" text="Lookup & Barcode" />
+            <DraggableItem type="HEADER" text="Header" />
+            <DraggableItem type="FOOTER" text="Footer" />
+          </div>
+          <div className="bottom left p-3" style={{ position: 'fixed', bottom: '0', left: '0', padding: '15px' }}>
+    <button onClick={downloadJsonFile} className="btn btn-light" style={{ color: 'black' }}>
+        Download JSON
+    </button>
+</div>
+
+
         </div>
 
         <div className="col-md-7 code-editor">
-          <h8 style={{ color:'white' }}>Drop Target Panel</h8>
+          <h8 style={{ color: 'white' }}>Drop Target Panel</h8>
           <DropTargetPanel droppedItems={droppedItems} setDroppedItems={setDroppedItems} onHover={handleHover} />
         </div>
 
         <div className="col-md-3 output-window">
-          <div className="bottom left p-3">
-            <button onClick={downloadJsonFile} className="bottom btn btn-dark">
-              Download JSON
-            </button>
-          </div>
-          <h8 style={{ color:'white' }}><b>Show Details</b></h8><br /><br />
-          {hoveredItemId && <span style={{ color:'white' }}><b>Field ID :</b>{hoveredItemId}</span>}<br />
-          {hoveredItemLabel && <span style={{ color:'white' }}><b>Field Label :</b> {hoveredItemLabel}</span>} <br />
-          {hoveredItemMandatory && <span style={{ color:'white' }}><b>IsMandatory :</b>{hoveredItemMandatory.toString()}</span>}<br />
-          {hoveredItemClass && <h8 style={{ color:'white' }}><b>Field Class :</b> {hoveredItemClass}</h8>} <br />
-          {hoveredItemValue && <h8 style={{ color:'white' }}><b>Field Value :</b> {hoveredItemValue}</h8>} <br />
-          {hoveredItemReadOnly && <h8 style={{ color:'white' }}><b>IsReadOnly :</b>{hoveredItemReadOnly.toString()}</h8>}
+
+          <h8 style={{ color: 'white' }}><b>Show Details</b></h8><br /><br />
+          {hoveredItemId && <span style={{ color: 'white' }}><b>Field ID :</b>{hoveredItemId}</span>}<br />
+          {hoveredItemLabel && <span style={{ color: 'white' }}><b>Field Label :</b> {hoveredItemLabel}</span>} <br />
+          {hoveredItemMandatory && <span style={{ color: 'white' }}><b>IsMandatory :</b>{hoveredItemMandatory.toString()}</span>}<br />
+          {hoveredItemClass && <h8 style={{ color: 'white' }}><b>Field Class :</b> {hoveredItemClass}</h8>} <br />
+          {hoveredItemValue && <h8 style={{ color: 'white' }}><b>Field Value :</b> {hoveredItemValue}</h8>} <br />
+          {hoveredItemReadOnly && <h8 style={{ color: 'white' }}><b>IsReadOnly :</b>{hoveredItemReadOnly.toString()}</h8>}
         </div>
-        
+
       </div>
     </div>
   );
